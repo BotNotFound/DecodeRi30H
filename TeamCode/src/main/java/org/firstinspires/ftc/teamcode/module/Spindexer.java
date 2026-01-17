a45e44e6978ea1a2d8f106856fc6367540ef21d6
FreehandBlock51 <freehandblock51@outlook.com>
2026-01-17 15:30:09 -0800

    tune spindexer offset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Spindexer.java
@@ -56,7 +56,7 @@ public class Spindexer {
     private SpindexerState curState;
 
     public static double ARTIFACT_DISTANCE_THRESHOLD_CM = 5;
-    public static double OFFSET_ANGLE = -23;
+    public static double OFFSET_ANGLE = -38;
     public static double[] INDICATOR_COLORS = {
         IndicatorColorValues.OFF,
         IndicatorColorValues.VIOLET,
